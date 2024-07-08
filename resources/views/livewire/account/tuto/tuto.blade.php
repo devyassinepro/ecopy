c5d4101811224805aff8ffb90c0f8e42d2fdd7e9
Touzani Yassine <devyassinepro@gmail.com>
2024-07-09 00:21:12 +0200

    make edits

--- resources/views/livewire/account/tuto/tuto.blade.php
+++ resources/views/livewire/account/tuto/tuto.blade.php
@@ -3,11 +3,11 @@
                         <div class="nk-content-inner">
                             <div class="nk-content-body">
 
-          <h2>How to Use Ecopy</h2>
+          <h2>How to Import Product To Shopify with Ecopy</h2>
         </br></br>
 
         <div class="table-responsive">
-        <iframe width="800" height="415" src="https://www.youtube.com/embed/Nn9MEKO2Uek?start=41" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>   </div>
+        <iframe width="800" height="415" src="https://www.youtube.com/embed/HF2A6wawT-o?si=qQZX9BcvSrRP5nJG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>   </div>
 
           <div>
 
