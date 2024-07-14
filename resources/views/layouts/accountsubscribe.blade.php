3bbbea41a8c309735d03031fdfb5c81cbd575a4a
Touzani Yassine <devyassinepro@gmail.com>
2024-07-14 13:43:51 +0200

    adding hotjar tag

--- resources/views/layouts/accountsubscribe.blade.php
+++ resources/views/layouts/accountsubscribe.blade.php
@@ -46,6 +46,17 @@
 
   gtag('config', 'G-234R6GX9KR');
 </script>
+<!-- Hotjar Tracking Code for Site 5058947 (nom manquant) -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5058947,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 <body>
     
