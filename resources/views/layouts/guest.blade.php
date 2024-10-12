6030a98489f5d44a2e4068eb5d88bfdb70ea0068
Touzani Yassine <devyassinepro@gmail.com>
2024-10-12 22:35:12 +0200

    fix nav menu

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -392,7 +392,7 @@ src="https://www.facebook.com/tr?id=1331014271011060&ev=PageView&noscript=1"
     
     <script src="assets/js/vendors/jquery-3.6.0.min.js"></script>
     <script src="{{ asset('assets/js/app.js') }}"></script>
-    <!-- <script src="assets/js/vendors/bootstrap.bundle.min.js"></script> -->
+    <script src="assets/js/vendors/bootstrap.bundle.min.js"></script>
     <script src="assets/js/vendors/swiper-bundle.min.js"></script>
     <script src="assets/js/vendors/parallax.min.js"></script>
     <script src="assets/js/vendors/jquery.magnific-popup.min.js"></script>
